ee7775636dad2b1096a3bcff1fcfd8683335dbe5
philDaprogrammer <prstanto@buffalo.edu>
2022-02-13 17:34:45 -0500

    works, additional features to be added

--- src/main.c
+++ src/main.c
@@ -13,7 +13,63 @@ typedef struct words {
 } words; 
 
 
-void playGame(const char *word) { 
+int isWord(const char *guess, words *wordInfo) { 
+   for (int i=0; i < wordInfo->bankSize; ++i) {
+        if (strcmp(guess, wordInfo->bank[i]) == 0) { 
+           return 1; 
+        }
+   } 
+
+   return 0; 
+}   
+
+
+int charPresent(char c, const char *word) { 
+   for (int i=0; i < WORD_SIZE; ++i) { 
+      if (c == word[i]) {return 1;}
+   }
+
+   return 0;  
+}  
+
+
+int *getCharacterOccurrence(const char *word) { 
+   int offset   = 0x60;
+   int *letters = (int *)calloc(26, sizeof(int));  
+
+   for (int i=0; i < WORD_SIZE; ++i) {
+      letters[word[i] - offset] += 1; 
+   } 
+
+   return letters; 
+} 
+
+
+void displayGuess(const char *word, const char *guess) { 
+   int *letters = getCharacterOccurrence(word); 
+   int offset   = 0x60; 
+
+   for (int i=0; i < WORD_SIZE; ++i) { 
+      printf("%c ", guess[i]); 
+   }
+   putchar('\n'); 
+
+   for (int i=0; i < WORD_SIZE; ++i) { 
+      if (word[i] == guess[i]) { 
+         printf("O "); 
+      } else if ((charPresent(guess[i], word)) && (letters[word[i] - offset] > 0)) { 
+         printf("_ ");  
+         letters[word[i] - offset] -= 1;
+      } else { 
+         printf("X "); 
+      } 
+   } 
+
+   printf("\n\n") ; 
+}  
+
+
+void playGame(const char *word, words *wordInfo) { 
    char *guess = (char *)malloc(WORD_SIZE + 1); 
    size_t  len = 0; 
    int i       = 0; 
@@ -26,15 +82,21 @@ void playGame(const char *word) {
          guess[strlen(guess)-1] = '\0'; 
       } 
 
-      if (strlen(guess) > WORD_SIZE) { 
-         printf("Guess is too long... Try again\n");
+      if ((strlen(guess) > WORD_SIZE) || (!isWord(guess, wordInfo))) { 
+         printf("Invalid guess... Try again\n\n");
          continue; 
       }
 
+      if (strcmp(word, guess) == 0) { 
+         printf("You Win!\n"); 
+         return; 
+      } 
+
+      displayGuess(word, guess); 
       i++; 
    }
 
-   printf("You loose\n"); 
+   printf("The word was: %s\n", word); 
 } 
 
 
@@ -47,11 +109,12 @@ char *getRandomWord(char **bank, int bankSize) {
 int main(int argc, char *argv[]) { 
    char *filePath         = "/home/phil/Programs/c/cse421/wordle/bank.txt"; 
    char *buffer           = readFile(filePath); 
-   struct words *wordInfo = (struct words *)malloc(sizeof(words)); 
+   words *wordInfo        = (words *)malloc(sizeof(words)); 
    wordInfo->bankSize     = getBankSize(buffer); 
    wordInfo->bank         = parse(buffer, wordInfo->bankSize);
+   char *randomWord       = getRandomWord(wordInfo->bank, wordInfo->bankSize); 
 
 
-   playGame(getRandomWord(wordInfo->bank, wordInfo->bankSize)); 
+   playGame(randomWord, wordInfo); 
    return 0; 
 } 
