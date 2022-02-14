feb2da3465c6e56e29f3b1758eadded8ad539994
philDaprogrammer <prstanto@buffalo.edu>
2022-02-14 14:58:07 -0500

    added installation script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+#### 
+# - Script to install wordle executable and any file dependencies 
+#
+####  
+
+
+if [ "$UID" != "0"]; then 
+   echo 'Must be root to place executable in $PATH' 
+   exit -1 
+fi 
+
+# - create config directory, bank.txt will be stored there
+if [ ! -f "~/.wordle" ]; then 
+   mkdir ~/.wordle 
+fi 
+
+mv bank.txt ~/.wordle     &&  
+gcc -o wordle src/main.c  && 
+mv wordle /usr/bin/wordle && 
+rm installs.sh Makefile README.md 
+rm -rdf src/
+
+exit 0
